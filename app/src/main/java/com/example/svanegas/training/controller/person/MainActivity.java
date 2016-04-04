62bdd919f3a490b018f13d71ea41463c3a923463
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-04-04 17:00:33 -0500

    Including a short example of support DrawerLayout

--- app/src/main/java/com/example/svanegas/training/controller/person/MainActivity.java
+++ app/src/main/java/com/example/svanegas/training/controller/person/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.svanegas.training.controller.person;
 
+import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.svanegas.training.R;
 import com.example.svanegas.training.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String SELECTED_ITEM_ID = "selected_item_id";
+
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private FloatingActionButton mFab;
+    private int mDrawerSelectedItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setupDrawer(toolbar);
+
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -37,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
         binding.content.peopleList.setLayoutManager(new LinearLayoutManager(this));
         binding.content.peopleList.setAdapter(new PersonAdapter(getLayoutInflater()));
 
-        /*Person person = new Person();
-        person.setFirstName("Santiago");
-        person.setLastName("Vanegas");
-        person.setAge(21);
-        binding.setPerson(person);*/
+        if (savedInstanceState == null) mDrawerSelectedItemId = R.id.drawer_item_1;
+        else mDrawerSelectedItemId = savedInstanceState.getInt(SELECTED_ITEM_ID);
+
+        navigateTo(mDrawerSelectedItemId);
+        setNavItemCount(R.id.drawer_item_4, 10);
     }
 
     @Override
@@ -64,5 +80,72 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (mDrawerToggle != null) mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void setupDrawer(Toolbar toolbar) {
+        mNavigationView = (NavigationView) findViewById(R.id.drawer_nav_view);
+        if (mNavigationView != null) mNavigationView.setNavigationItemSelectedListener(this);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    private void navigateTo(int destination) {
+
+    }
+
+    private void setNavItemCount(int itemId, int count) {
+        if (mNavigationView != null && mNavigationView.getMenu() != null) {
+            MenuItem menuItem = mNavigationView.getMenu().findItem(itemId);
+            if (menuItem != null) {
+                TextView view = (TextView) menuItem.getActionView();
+                if (view != null) view.setText(count > 0 ? String.valueOf(count) : null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        if (item == null || mDrawerLayout == null || mFab == null) return false;
+        item.setChecked(true);
+        mDrawerSelectedItemId = item.getItemId();
+        if (item.getItemId() == R.id.drawer_item_1) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            Snackbar.make(mFab, "Clicked first", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        if (item.getItemId() == R.id.drawer_item_2) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+            Snackbar.make(mFab, "Clicked second", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_ITEM_ID, mDrawerSelectedItemId);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
